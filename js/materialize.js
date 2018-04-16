cf4651bfb555fc73edff6dca19c2c54121798d38
Laawe <laura.render.03@gmail.com>
2018-04-15 20:03:16 -0500

    splash screen

--- js/materialize.js
+++ js/materialize.js
@@ -1,4 +1,14 @@
+// vista splash
+$(document).ready(function () {
+    setTimeout(function () {
+        $("#splash").fadeOut(500);
+    }, 3000); //oculta la pantalla inicial
+    setTimeout(function () {
+        $("#main").fadeIn(500);
+    }, 3000); /*Muestra la pantalla Principal*/
+});
 // tabs
 $(document).ready(function () {
     $('ul.tabs').tabs();
 });
+
